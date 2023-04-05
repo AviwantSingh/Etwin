51fb717bd93aeb273f80a27db33d8e2afc8246e0
Aviwant kumar <avi259@gmail.com>
2023-04-05 07:28:04 +0530

    Code changes for invalid credentials

--- Etwin1/Controllers/LoginController.cs
+++ Etwin1/Controllers/LoginController.cs
@@ -9,6 +9,10 @@ using Microsoft.AspNetCore.Http;
 using System.Net.Http;
 
 using System.Web;
+using EtwLogin.Models;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+
+
 namespace Etwin1.Controllers
 {
     public class LoginController : Controller
@@ -33,6 +37,7 @@ namespace Etwin1.Controllers
        
         public ActionResult Loginview(string username, string Password)
         {
+            Boolean issucess = false;
             var issuccess = _service.AuthenticateUser(username, Password);
 
             if(username==null)
@@ -42,19 +47,13 @@ namespace Etwin1.Controllers
             if (issuccess.Result != null)
             {
                 string name = HttpContext.Session.GetString(username);
-                //ViewBag.username = string.Format("Successfully logged-in", username);
-
-                //TempData["username"] = username;
-                //TempData["TempModel"] = "test";
                 TempData["Key"] = username;
-                //ViewBag.issuccess = issuccess;
-                return RedirectToAction("Welcome");
+                return RedirectToAction("Department");
             }
             else
             {
                 ViewBag.username = string.Format("Login Failed ", username);
-               
-                return View("Loginview");
+                return Json(new { Message = ViewBag.username, System.Web.Mvc.JsonRequestBehavior.AllowGet });
             }
            
         }
@@ -66,7 +65,7 @@ namespace Etwin1.Controllers
             TempData.Keep("Key");
             return View();
         }
-        public ActionResult Department()
+        public ViewResult Department()
         {
 
             var data = TempData["Key"];
